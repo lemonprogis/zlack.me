bcb711a7cdcf8a2b7fac2d6a2a681f7a031f2b46
Ed Briggler <ed.briggler@jbhunt.com>
2023-09-15 11:13:25 -0500

    adding a default 404

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,15 +4,17 @@ import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import CreateRoom from './pages/CreateRoom';
 import Room from './pages/Room';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
     <>
     <Navigation />
     <Routes>
-       <Route path="/" element={<Home />} />
-       <Route path="/create-room" exact element={<CreateRoom />} />
+       <Route exact path="/" element={<Home/>} />
+       <Route exact path="/create-room" element={<CreateRoom />} />
        <Route path="/room/:roomID" element={<Room />} />
+       <Route path="*" element={<NotFoundPage />} />
     </Routes>
     <Footer />
     </>
---
+++ ui/src/pages/NotFoundPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ZlackLogo from '../components/ZlackLogo';
+
+function NotFoundPage() {
+    return (
+        <>
+        <section class="bg-light py-5">
+            <div class="container px-5">
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-xxl-8">
+                        <div class="text-center my-5">
+                            <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">404</span></h2>
+                            <p class="lead fw-light mb-4">Err....nothing is here!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+    );
+}
+
+export default NotFoundPage;
