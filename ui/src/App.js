d226077c9572d81ea5e9c19370274acb02f1d439
Ed Briggler <ed.briggler@jbhunt.com>
2023-09-19 21:24:30 -0500

    Revert "styling room"
    This reverts commit d3b64d365f3b31cf6dec65c01f697bf5339936c1.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,17 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 import CreateRoom from './pages/CreateRoom';
 import Room from './pages/Room';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
+    <>
+    <Navigation />
     <Routes>
        <Route exact path="/" element={<Home/>} />
        <Route exact path="/create-room" element={<CreateRoom />} />
        <Route path="/room/:roomID" element={<Room />} />
        <Route path="*" element={<NotFoundPage />} />
     </Routes>
+    <Footer />
+    </>
   );
 }
 
--- ui/src/pages/NotFoundPage.js
+++ ui/src/pages/NotFoundPage.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import PageWrapper from '../components/PageWrapper';
 
 function NotFoundPage() {
     return (
-        <PageWrapper>
-            <section class="bg-light py-5">
-                <div class="container px-5">
-                    <div class="row gx-5 justify-content-center">
-                        <div class="col-xxl-8">
-                            <div class="text-center my-5">
-                                <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">404</span></h2>
-                                <p class="lead fw-light mb-4">Err....nothing is here!</p>
-                            </div>
+        <>
+        <section class="bg-light py-5">
+            <div class="container px-5">
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-xxl-8">
+                        <div class="text-center my-5">
+                            <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">404</span></h2>
+                            <p class="lead fw-light mb-4">Err....nothing is here!</p>
                         </div>
                     </div>
                 </div>
-            </section>
-        </PageWrapper>
+            </div>
+        </section>
+    </>
     );
 }
 
